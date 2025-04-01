a01fc0bedd0a843cba100c3e2fbe6e4e96164b0f
Lucien <davi83242@gmail.com>
2025-04-01 14:29:29 -0300

    organizando

--- conteudo/conf_perfil/conf_perfil.js
+++ conteudo/conf_perfil/conf_perfil.js
@@ -1,18 +1,3 @@
-<<<<<<< Updated upstream
-function abrirNotifi(){
-	if (down) {
-		box.style.height  = '0px';
-		box.style.opacity = 0;
-		down = false;
-	}else {
-		box.style.height  = '510px';
-		box.style.opacity = 1;
-		down = true;
-	}
-}
-
-=======
->>>>>>> Stashed changes
 function abrirMenuConf() {
     const configMenu = document.getElementById('configMenu');
     if (configMenu.style.display === 'none' || configMenu.style.display === '') {
@@ -20,8 +5,6 @@ function abrirMenuConf() {
     } else {
         configMenu.style.display = 'none'; // Oculta o submenu
     }
-<<<<<<< Updated upstream
-=======
 }
 
 // Para ocultar o menu ao clicar fora dele (opcional)
@@ -32,5 +15,4 @@ window.onclick = function(event) {
             configMenu.style.display = 'none';
         }
     }
->>>>>>> Stashed changes
 }

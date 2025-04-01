b58e6e8905d88784c3b7e0e7ee866d68c0b970f9
Lucien <davi83242@gmail.com>
2025-04-01 13:20:11 -0300

    carrossel de imagens

---
+++ conteudo/inicio/script.js
@@ -0,0 +1,49 @@
+
+let index = 0; 
+const imagens = document.querySelector('.imagens');
+const totalImagens = document.querySelectorAll('.imagens img').length;
+
+function mudarImagem() {
+    index = (index + 1) % totalImagens; 
+    const deslocamento = -index * 600; 
+    imagens.style.transform = `translateX(${deslocamento}px)`;
+}
+
+// mundando a imagem a cada 3 segundos
+setInterval(mudarImagem, 3000);
+
+
+var box  = document.getElementById('box');
+var down = false;
+
+
+function abrirNotifi(){
+	if (down) {
+		box.style.height  = '0px';
+		box.style.opacity = 0;
+		down = false;
+	}else {
+		box.style.height  = '510px';
+		box.style.opacity = 1;
+		down = true;
+	}
+}
+
+function abrirMenuConf() {
+    const configMenu = document.getElementById('configMenu');
+    if (configMenu.style.display === 'none' || configMenu.style.display === '') {
+        configMenu.style.display = 'block'; // Mostra o submenu
+    } else {
+        configMenu.style.display = 'none'; // Oculta o submenu
+    }
+}
+
+// Para ocultar o menu ao clicar fora dele (opcional)
+window.onclick = function(event) {
+    if (!event.target.matches('.itens')) {
+        const configMenu = document.getElementById('configMenu');
+        if (configMenu.style.display === 'block') {
+            configMenu.style.display = 'none';
+        }
+    }
+}

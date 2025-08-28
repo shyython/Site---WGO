287ca42dea02bda385b7b9f091a25e730a400232
shyython <beatriz.follow@gmail.com>
2025-08-27 23:03:57 -0300

    .

--- conteudo/conf_perfil/Configuracao_Perfil.php
+++ conteudo/conf_perfil/Configuracao_Perfil.php
@@ -1,29 +1,3 @@
- 
-<?php
-if(isset($_FILES['arquivos'])) {
-    $arquivo = $_FILES['arquivos'];
-
-    if($arquivo['error'])
-    die("Falha ao enviar o arquivo, tente novamente.");
-
-    $pasta = "arquivos/";
-    $nomeDoArquivo = $arquivo["name"];
-    $novoNomeDoArquivo = uniqid();
-    $extensao = strtolower(pathinfo($nomeDoArquivo, PATHINFO_EXTENSION));
-
-    if ($extensao != "jpg" && $extensao != 'png')
-    die("Tipo de arquivo não aceito.");
-
-    $deu_certo = move_uploaded_file($arquivo["temp_name"], $pasta . $novoNomeDoArquivo . "." . $extensao);
-        if($deu_certo)
-        echo "<p>Arquivo enviado com sucesso! Para acessá-lo, <a target=\"_blank\" href=\"arquivos/$novoNomeDoArquivo.$extensao";
-        else 
-        echo "<p>Falha ao enviar arquivo</p>";
-    
-}
-?>
- 
-
 <!DOCTYPE html>
 <html lang="en">
 

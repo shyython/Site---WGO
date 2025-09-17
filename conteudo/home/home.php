a04b2a852aadf26b237355a5b00a311901031407
Ella Prado <gabinathcarol@gmail.com>
2025-09-17 11:50:45 -0300

    .

--- conteudo/home/home.php
+++ conteudo/home/home.php
@@ -7,7 +7,8 @@
   <link rel="stylesheet" href="homee.css">
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
   <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/3.0.0/uicons-brands/css/uicons-brands.css'>
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
   <title>Bem-vindo ao WGO!</title>
@@ -101,9 +102,9 @@
       <div class="slides"></div>
       <div class="indicadores"></div>
     </section>
-<!--select estado-->
-  <div class="selectEstado1">
-    <select id="estado1">
+    <!--select estado-->
+    <div class="selectEstado1">
+      <select id="estado1">
         <option value="" disabled selected class="selectTxt">Selecione seu Estado</option>
         <option value="AC">Acre</option>
         <option value="AL">Alagoas</option>
@@ -132,136 +133,126 @@
         <option value="SP">São Paulo</option>
         <option value="SE">Sergipe</option>
         <option value="TO">Tocantins</option>
-    </select>
-  </div>
-<!--categorias-->
-  <div class="categoria">
-  <div class="categoria2" 
-       janela-title="Baladas" 
-       janela-descri="As melhores baladas da cidade para você curtir a noite." 
-       janela-img="../imagens/home/baladaHome.webp">
-    <img src="../imagens/home/baladaHome.webp" alt="Baladas" class="imgCategoria">
-    <p class="txtCategoria">Baladas</p>
-  </div>
-
-  <div class="categoria2" 
-       janela-title="Esportes" 
-       janela-descri="Locais e eventos esportivos para praticar e assistir." 
-       janela-img="../imagens/home/esportesHome.jpg">
-    <img src="../imagens/home/esportesHome.jpg" alt="Esportes" class="imgCategoria">
-    <p class="txtCategoria">Esportes</p>
-  </div>
-
-  <div class="categoria2" 
-       janela-title="Restaurantes" 
-       janela-descri="Restaurantes variados para todos os gostos." 
-       janela-img="../imagens/home/restaurantesHome.jpg">
-    <img src="../imagens/home/restaurantesHome.jpg" alt="Restaurantes" class="imgCategoria">
-    <p class="txtCategoria">Restaurantes</p>
-  </div>
-
-  <div class="categoria2" 
-       janela-title="Cultura" 
-       janela-descri="Experiências culturais e artísticas para todos." 
-       janela-img="../imagens/home/culturaHome.jpg">
-    <img src="../imagens/home/culturaHome.jpg" alt="Cultura" class="imgCategoria">
-    <p class="txtCategoria">Cultura</p>
-  </div>
-
-  <div class="categoria2" 
-       janela-title="Eventos Comunitários" 
-       janela-descri="Participe de eventos locais e comunitários." 
-       janela-img="../imagens/home/eventosComuniHome.jpg">
-   <img src="../imagens/home/eventosComuniHome.jpg" alt="Pontos Turísticos e Históricos" class="imgCategoria">
-   <p class="txtCategoria">Eventos Comunitários</p>
-
-  </div>
-
-  <div class="categoria2" 
-       janela-title="Pontos Turísticos e Históricos" 
-       janela-descri="Conheça os principais pontos turísticos e históricos da região." 
-       janela-img="../imagens/home/historicoHome.webp">
-    <img src="../imagens/home/historicoHome.webp" alt="Pontos Turísticos e Históricos" class="imgCategoria">
-    <p class="txtCategoria">Pontos Turísticos e Históricos</p>
-  </div>
-
-  <div class="categoria2" 
-       janela-title="Praias e Ar Livre" 
-       janela-descri="Aproveite praias e atividades ao ar livre." 
-       janela-img="../imagens/home/praiaHome.jpg">
-    <img src="../imagens/home/praiaHome.jpg" alt="Praias e Ar Livre" class="imgCategoria">
-    <p class="txtCategoria">Praias e Ar Livre</p>
-  </div>
-</div>
-
-
-<div class="container-janela">
-  <div class="janelaInfo" id="janela">
-    <h2 id="janelaTitle">Título inicial</h2>
-
-    <img id="janelaImg" src="" alt="Imagem da categoria">
-
-      <button id="esquerda-button" aria-label="Slide para a esquerda"> 
-      <img src="../imagens/home/arrow2.png" alt="seta esquerda" />
-      </button>
-
-      <div class="janela-img"></div>
-      <button id="direita-button" aria-label="Slide para a direita">
-      <img src="../imagens/home/arrow.png" alt="seta direita" />
-      </button>
-
-    <main class="janela-galeria" role="main" aria-label="Galeria de imagens" style="display:none;">
-
-      <aside class="destaques-janela">
-        <h2 class="titulo-destaque">Título destaque</h2>
-        <p class="descricao">Descrição destaque</p>
-      </aside>
-      
-    </main>
-
-    <p id="janelaContent">Conteúdo da janela</p>
-    <button id="btnFechar">Fechar</button>
-  </div>
-</div>
-
-  <div class="rodape">
-    <ul class="rodape-icons">
-      <a href="#"><img src="../imagens/home/facebook.png" alt="Facebook" class="icon-rodape"></a>
-      <a href="#"><img src="../imagens/home/insta.png" alt="Instagram" class="icon-rodape"></a>
-      <a href="#"><img src="../imagens/home/github.png" alt="Github" class="icon-rodape"></a>
-    </ul>
-    <div class="dados-rodape">
-      <p>Av. Conselheiro Nébias, 309 - Vila Mathias - Santos - SP</p>
-      <p style="margin-top: 10px;">© 2025 WGO. O mundo é vasto e cheio de surpresas – permita-se descobrir, sentir e se apaixonar por novos lugares.</p>
-
-<div class="contato-rodape">
-
-  <div class="telefone-rodape">
-      <img src="../imagens/home/telefone.png" alt="" style="width: 20px; height: auto;">
-      <p>+55 13930801523</p>
-  </div>
-
-  <div class="email-rodape">
-      <img src="../imagens/home/email.png" alt="" style="width: 20px;">
-      <p>wegooutturimo@hotmail.com</p>
-  </div>
-
-</div>
-
-      <div class="logo-rodape">
-        <img src="../imagens/WgoIcon2.png" alt="" style="width: 200px; height: auto;">
+      </select>
+    </div>
+    <!--categorias-->
+    <div class="categoria">
+      <div class="categoria2" janela-title="Baladas"
+        janela-descri="As melhores baladas da cidade para você curtir a noite."
+        janela-img="../imagens/home/baladaHome.webp">
+        <img src="../imagens/home/baladaHome.webp" alt="Baladas" class="imgCategoria">
+        <p class="txtCategoria">Baladas</p>
       </div>
 
-    </div>
-  </div>
+      <div class="categoria2" janela-title="Esportes"
+        janela-descri="Locais e eventos esportivos para praticar e assistir."
+        janela-img="../imagens/home/esportesHome.jpg">
+        <img src="../imagens/home/esportesHome.jpg" alt="Esportes" class="imgCategoria">
+        <p class="txtCategoria">Esportes</p>
+      </div>
 
-</main>
-    <script>
-        $(document).ready(function() {
-            $('#estado').select2();
-        });
-    </script>
- 
+      <div class="categoria2" janela-title="Restaurantes" janela-descri="Restaurantes variados para todos os gostos."
+        janela-img="../imagens/home/restaurantesHome.jpg">
+        <img src="../imagens/home/restaurantesHome.jpg" alt="Restaurantes" class="imgCategoria">
+        <p class="txtCategoria">Restaurantes</p>
+      </div>
+
+      <div class="categoria2" janela-title="Cultura" janela-descri="Experiências culturais e artísticas para todos."
+        janela-img="../imagens/home/culturaHome.jpg">
+        <img src="../imagens/home/culturaHome.jpg" alt="Cultura" class="imgCategoria">
+        <p class="txtCategoria">Cultura</p>
+      </div>
+
+      <div class="categoria2" janela-title="Eventos Comunitários"
+        janela-descri="Participe de eventos locais e comunitários." janela-img="../imagens/home/eventosComuniHome.jpg">
+        <img src="../imagens/home/eventosComuniHome.jpg" alt="Pontos Turísticos e Históricos" class="imgCategoria">
+        <p class="txtCategoria">Eventos Comunitários</p>
+
+      </div>
+
+      <div class="categoria2" janela-title="Pontos Turísticos e Históricos"
+        janela-descri="Conheça os principais pontos turísticos e históricos da região."
+        janela-img="../imagens/home/historicoHome.webp">
+        <img src="../imagens/home/historicoHome.webp" alt="Pontos Turísticos e Históricos" class="imgCategoria">
+        <p class="txtCategoria">Pontos Turísticos e Históricos</p>
+      </div>
+
+      <div class="categoria2" janela-title="Praias e Ar Livre"
+        janela-descri="Aproveite praias e atividades ao ar livre." janela-img="../imagens/home/praiaHome.jpg">
+        <img src="../imagens/home/praiaHome.jpg" alt="Praias e Ar Livre" class="imgCategoria">
+        <p class="txtCategoria">Praias e Ar Livre</p>
+      </div>
+    </div>
+
+
+    <div class="container-janela">
+      <div class="janelaInfo" id="janela">
+        <h2 id="janelaTitle">Título inicial</h2>
+
+        <img id="janelaImg" src="" alt="Imagem da categoria">
+
+        <button id="esquerda-button" aria-label="Slide para a esquerda">
+          <img src="../imagens/home/arrow2.png" alt="seta esquerda" />
+        </button>
+
+        <div class="janela-img"></div>
+        <button id="direita-button" aria-label="Slide para a direita">
+          <img src="../imagens/home/arrow.png" alt="seta direita" />
+        </button>
+
+        <main class="janela-galeria" role="main" aria-label="Galeria de imagens" style="display:none;">
+
+          <aside class="destaques-janela">
+            <h2 class="titulo-destaque">Título destaque</h2>
+            <p class="descricao">Descrição destaque</p>
+          </aside>
+
+        </main>
+
+        <p id="janelaContent">Conteúdo da janela</p>
+        <button id="btnFechar">Fechar</button>
+      </div>
+    </div>
+
+    <div class="rodape">
+      <ul class="rodape-icons">
+        <a href="#"><img src="../imagens/home/facebook.png" alt="Facebook" class="icon-rodape"></a>
+        <a href="#"><img src="../imagens/home/insta.png" alt="Instagram" class="icon-rodape"></a>
+        <a href="#"><img src="../imagens/home/github.png" alt="Github" class="icon-rodape"></a>
+      </ul>
+      <div class="dados-rodape">
+        <p>Av. Conselheiro Nébias, 309 - Vila Mathias - Santos - SP</p>
+        <p style="margin-top: 10px;">© 2025 WGO. O mundo é vasto e cheio de surpresas – permita-se descobrir, sentir e
+          se apaixonar por novos lugares.</p>
+
+        <div class="contato-rodape">
+
+          <div class="telefone-rodape">
+            <img src="../imagens/home/telefone.png" alt="" style="width: 20px; height: auto;">
+            <p>+55 13930801523</p>
+          </div>
+
+          <div class="email-rodape">
+            <img src="../imagens/home/email.png" alt="" style="width: 20px;">
+            <p>wegooutturimo@hotmail.com</p>
+          </div>
+
+        </div>
+
+        <div class="logo-rodape">
+          <img src="../imagens/WgoIcon2.png" alt="" style="width: 200px; height: auto;">
+        </div>
+
+      </div>
+    </div>
+
+  </main>
+  <script>
+    $(document).ready(function () {
+      $('#estado').select2();
+    });
+  </script>
+
   <p id="legenda" style="text-align:center; margin-top: 10px; font-weight:bold;"></p>
   <script src="script.js"></script>
 </body>
